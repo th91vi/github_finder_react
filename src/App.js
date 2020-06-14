b0ed20e38b6b17b12dc3a441b05354df9d87f733
Thiago Vieira <th91v@outlook.com>
2020-06-14 13:53:24 -0300

    experimentos com declarações e condicionais em JSX

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    // userName = () => { return 'John Pessoa' };
+
     render() {
+        const userName = 'John Person';
+        const loading = false;
+        const showName = true;
+
+        // if (loading) {
+        //     return <h2>Loading...</h2>
+        // }
+
         return (
             <div className="App">
                 <h1>Github React Finder</h1>
+                <ul>
+                {loading ? <h4>Loading...</h4> : <li>User: {showName && userName }</li>}
+                </ul>
             </div>
         );
-        // return (
-        //     <>
-        //         <h1>Github React Finder</h1>
-        //     </>
-        // );
-
-        // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Github React Finder'));
-        // return createElement('div', {className:'App'}, createElement('h1', null, 'Github React Finder'));
     }
 }
 
