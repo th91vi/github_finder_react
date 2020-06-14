6f9db5dd95046488c1f8d96c35d14a82f6a1ce47
Thiago Vieira <th91v@outlook.com>
2020-06-14 13:17:07 -0300

    experimentos com JSX, com React.createElement; limpeza do manifest.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +8,14 @@ class App extends Component {
                 <h1>Github React Finder</h1>
             </div>
         );
+        // return (
+        //     <>
+        //         <h1>Github React Finder</h1>
+        //     </>
+        // );
+
+        // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Github React Finder'));
+        // return createElement('div', {className:'App'}, createElement('h1', null, 'Github React Finder'));
     }
 }
 
