bfa0d2b96a06d29314ebc3b245c7084b6cfc3a77
Thiago Vieira <th91v@outlook.com>
2020-06-26 21:00:06 -0300

    Lógica para limpar usuários da tela e condição para exibir botão. Destructuring de props em App e Search. Condição para exibir alerta caso ocorra submit com o campo de busca vazio, para evitar errro de "map undefined"

--- src/App.js
+++ src/App.js
@@ -10,15 +10,6 @@ class App extends Component {
         loading: false
     }
 
-    // async componentDidMount() {
-    //     this.setState({ loading: true })
-
-    //     const usersResponse = await fetch(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    //     const usersResult = await usersResponse.json();
-
-    //     this.setState({ users: usersResult, loading: false })
-    // }
-
     searchUsers = async (text) => {
         this.setState({ loading: true })
 
@@ -28,14 +19,19 @@ class App extends Component {
         this.setState({ users: usersResult.items, loading: false })
     }
 
+    clearUsers = () => {
+        this.setState({ users: [], loading: false});
+    }
+
     render() {
+        const { users, loading } = this.state;
 
         return (
             <div className="App">
                 <Navbar />
                 <div className='container'>
-                    <Search searchUsers={this.searchUsers} />
-                    <Users loading={this.state.loading} users={this.state.users} />
+                    <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false}/>
+                    <Users loading={loading} users={users} />
                 </div>
             </div>
         );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,24 +7,35 @@ export class Search extends Component {
     };
 
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
     }
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' });
+        if (this.state.text === '') {
+            alert('Please insert a username')
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        }
     }
 
     onChange = e => this.setState({ [e.target.name]: e.target.value })
 
     render() {
+        const { showClear, clearUsers } = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className='form'>
                     <input type='text' name='text' placeholder='Search users...' value={this.state.text} onChange={this.onChange} />
                     <input type='submit' value='Search' className='btn btn-dark btn-block' />
                 </form>
+                {showClear && (
+                    <button className='btn btn-light btn-block' onClick={clearUsers}>Clear</button>
+                )}
             </div>
         )
     }
